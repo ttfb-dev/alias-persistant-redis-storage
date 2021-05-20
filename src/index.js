7805dde5737cf15e39f47bc1feb6e7f25b092764
Orange <bobrov.d.p@readyforsky.com>
2021-05-20 20:07:21 +0300

    add room and app storage

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const port = 80;
 app.use(bodyParser.json());
 
 const prefix_user = 'prs_user';
+const prefix_room = 'prs_room';
+const prefix_app  = 'prs_app';
 
 app.post('/user/:user_id([\\d])/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
   await persistentRedisService.set(prefix_user, `${req.params.user_id}_${req.params.parameter}`, req.body)
@@ -15,6 +17,32 @@ app.post('/user/:user_id([\\d])/parameter/:parameter([a-z\\d\_]{1,50})', async (
 
 app.get('/user/:user_id([\\d])/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
   const answ = await persistentRedisService.get(prefix_user, `${req.params.user_id}_${req.params.parameter}`);
+  if (answ) {
+    return res.status(200).json(answ);
+  }
+  return res.status(404).send();
+});
+
+app.post('/room/:room_id/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
+  await persistentRedisService.set(prefix_room, `${req.params.room_id}_${req.params.parameter}`, req.body)
+  return res.status(200).send();
+});
+
+app.get('/room/:room_id/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
+  const answ = await persistentRedisService.get(prefix_user, `${req.params.room_id}_${req.params.parameter}`);
+  if (answ) {
+    return res.status(200).json(answ);
+  }
+  return res.status(404).send();
+});
+
+app.post('/app/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
+  await persistentRedisService.set(prefix_app, `app_${req.params.parameter}`, req.body)
+  return res.status(200).send();
+});
+
+app.get('/app/parameter/:parameter([a-z\\d\_]{1,50})', async (req, res) => {
+  const answ = await persistentRedisService.get(prefix_app, `${req.params.parameter}`);
   if (answ) {
     return res.status(200).json(answ);
   }
